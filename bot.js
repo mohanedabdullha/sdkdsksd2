c7438479dc523ebbe0053457139188f8b1875e5c
mohanedabdullha <43791989+mohanedabdullha@users.noreply.github.com>
2018-11-06 19:47:49 +0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+
+client.on('ready', async() => {
+var server = "508953530317668352"; // ايدي السررفر
+var channel = "509383732600504340";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , Magic Spam , **')
+    },305);
+})
+
+client.login(process.env.BOT_TOKEN)
